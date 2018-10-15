bd49f084366ed63574513b612330bef5b6b035de
DESKTOP-AKFHO5F\Connor <connor_fong@hotmail.com>
2018-10-14 23:42:24 -0700

    43 - added libraries (React, Webpack, Babel), build function, start function, clean function, webpage, and set to build node_modules and dist in root (modified gitignore)

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const moduleObj = {
+    loaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loaders: ["babel-loader"],
+        }
+    ],
+};
+
+const client = {
+    entry: {
+        'client': './src/client/index.js',
+    },
+    target: 'web',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist/public')
+    },
+    module: moduleObj,
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'src/client/index.html'
+        })
+    ]
+};
+
+const server = {
+    entry: {
+        'server': './src/server/index.js'
+    },
+    target: 'node',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: moduleObj,
+    externals: [nodeExternals()]
+}
+
+module.exports = [client, server];
