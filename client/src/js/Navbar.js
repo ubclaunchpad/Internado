e6dd1bb50e72844479a32c417a175e548a0e75ca
Cormac Mollitor <cormacjmollitor@gmail.com>
2018-10-20 14:28:00 -0700

    Finished Initial Skeleton for Navbar
    Will add API calls for search soon and remove any unneeded code

--- client/src/js/Navbar.js
+++ client/src/js/Navbar.js
@@ -2,73 +2,67 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 class Navbar extends Component {
-  state = {
-    data: null
-  };
-/*
-  componentDidMount() {
-    this.callBackendAPI()
-    .then(res => this.setState({data: res.express}))
-    .catch(err => console.log(err));
+  constructor(){
+    super();
+    this.state = {
+      searchCategory: "Select Category"
+    };
+    this.searchCategories = ["Position", "Location"]
+    this.updateSearchCategory = this.updateSearchCategory.bind(this);
   }
 
-  callBackendAPI = async() => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+  updateSearchCategory(newCategory){
+    this.setState({
+      searchCategory: newCategory
+    });
+  }
+  /*
+    state = {
+      search_category: "Select Category"
+    };
 
-    if (response.status !== 200) {
-      throw Error(body.message);
+    componentDidMount() {
+      this.callBackendAPI()
+      .then(res => this.setState({data: res.express}))
+      .catch(err => console.log(err));
     }
 
-    return body;
-  }
-*/
+    callBackendAPI = async() => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+
+      if (response.status !== 200) {
+        throw Error(body.message);
+      }
+
+      return body;
+    }
+  */
   render() {
     return (
       <div className="Navbar">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
           <a class="navbar-brand" href="#">Internado</a>
+
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <input class="form-control mr-sm-2" type="search" placeholder="Search Companies" aria-label="Search"/>
             <div class="dropdown">
               <button class="btn btn-secondary dropdown-toggle category-dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown button
+                {this.state.searchCategory}
               </button>
               <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" onClick={() => {this.updateSearchCategory(this.searchCategories[0])}}>{this.searchCategories[0]}</a>
+                <a class="dropdown-item" onClick={() => {this.updateSearchCategory(this.searchCategories[1])}} >{this.searchCategories[1]}</a>
               </div>
             </div>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
+            <div class="navbar-nav ml-auto">
+              <button type="button" class="btn btn-primary mr-2">Sign up</button>
+              <button type="button" class="btn btn-secondary">Login</button>
+            </div>
           </div>
         </nav>
       </div>
