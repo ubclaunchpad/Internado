31f442ccddbab22d6a96aae3df7c4846f0e54000
Michael <michaelkxian@gmail.com>
2018-10-23 00:19:56 -0700

    Make the table more beautiful and add feautres
    Made the table look as similar to the wireframe as I could. Items can
    now be selected. I also added the original posting/apply buttons. There
    is an excerpt that shows before being selected, and the full description
    is shown after being selected. From what I can tell from the wireframe,
    the location is supposed to disappear when selected. This has been
    implemented. Also added fontAwesome stylesheet, but I can't figure out
    how to import it, so it's just kinda there for when I figure out later,
    so I can make the open table arrow look better.

--- client/src/js/ResultsTable.js
+++ client/src/js/ResultsTable.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 
+
 class Result {
-    constructor(title, description, location, url) {
+    constructor(title, description, excerpt, location, postUrl, applyUrl) {
     this.title = title;
     this.description = description;
+    this.excerpt = excerpt;
     this.location = location;
-    this.url = url;
+    this.postUrl = postUrl;
+    this.applyUrl = applyUrl;
+    this.selected = false;
+    this.i = 0;
   }
 }
 
@@ -18,27 +23,53 @@ class ResultsTable extends Component {
       }
   }
 
-  handleClick(url) {
-    window.open(url, "_blank");
+  handleClick(action, result) {
+    switch (action) {
+      case 0: // Clicked on row to select/deselect
+        result.selected = !result.selected;
+        this.setState({
+            results: this.state.results,
+          }); // result.selected = !result.selected
+        break;
+      case 1: // Clicked on original post
+        window.open(result.postUrl, '_blank');
+        break;
+      case 2: // Clicked on apply
+        window.open(result.applyUrl);
+        break;
+    }
   }
 
   renderResult(result) {
+    let text = result.selected ? result.description : result.excerpt; // If selected, show description, otherwise, show excerpt
+    let desc = <p class="table-result-desc"> {text} </p>; // Description/excerpt
+    let arrow = ">";
+    let originalPosting = "";
+    let applyButton = "";
+    let location = result.location;
+    if (result.selected) {
+      location = ""; // Hide things that shouldnt be shown when selected, show things that should be shown
+      arrow = "";
+      originalPosting = <a class="table-post clickable" onClick={()=>this.handleClick(1, result)}><br/> Original Posting</a>;
+      applyButton = <button class="table-button clickable" onClick={()=>this.handleClick(2, result)}>Apply</button>;
+    }
     return (
-      <tr onClick={()=>this.handleClick(result.url)} class="table-row" paddingTop="10px">
-        <td align="left" class="table-cell-1 table-cell"> <h5> {result.title} </h5>  {result.description} </td>
-        <td class="table-cell-2 table-cell"> <h5><br /></h5> {result.location} </td>
-        <td class="table-cell-3 table-cell"> <h5><br /></h5> &gt; </td>
+      <tr onClick={()=>this.handleClick(0, result)} class="table-row" paddingTop="10px">
+        <td align="left" class="table-cell-1 table-cell"> <h5 class="table-result-title"> {result.title} </h5>  {desc} </td>
+        <td class="table-cell-2 table-cell" align="right">  <p class="table-result-location">{location}</p> {originalPosting} </td>
+        <td class="table-cell-3 table-cell">  <p class="table-arrow">{arrow}{applyButton}</p> </td>
       </tr>
     );
   }
+
   render() {
     var rows = [];
     for (var i = 0; i < this.state.results.length; i++) {
-      rows.push(this.renderResult(this.state.results[i]));
-      rows.push(<br/>)
+      this.state.results[i].i = i;  // Make i available from the result object
+      rows.push(this.renderResult(this.state.results[i])); // Render each row
     }
     return (
-        <table>
+        <table class="results-table">
           {rows}
         </table>
       );
