feb26634bf8fce7718db2ba596de0f315956cb98
Cormac Mollitor <cormacjmollitor@gmail.com>
2018-11-10 13:21:59 -0800

    Got redirect working from the Home screen. Once we have the emails table up in the backend, we can make an API call to it to check if the email exists. If it doesn't, we can make a record and then redirect to the results screen.
    Relates to issue #96

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router";
+
 import "../styles/Home.css";
 
 export default class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            value: '',
+            redirect: false
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.navigateToResults = this.navigateToResults.bind(this);
     }
 
     handleChange(event) {
         this.setState({value: event.target.value});
     }
 
+    navigateToResults(){
+        this.setState({redirect: true});
+    }
+
     handleSubmit(event) {
+        // Make a change here to POST email to the email table
+        // If the email exists, alert the user on this page
+        // Otherwise, create an email record and transition to the results table
         alert('An email was submitted: ' + this.state.value);
         event.preventDefault();
     }
 
+    // We should probably refactor the two buttons' className fields to
+    // something like "homeButton" or something
     render() {
+        if(this.state.redirect) return <Redirect to='/results' />
         return (
             <div className="Home">
                 <div className="lander">
@@ -37,10 +53,13 @@ export default class Home extends Component {
                                type="email"
                                placeholder="searcher@mail.com"
                                aria-label="Email"
-                               onChange="5"
+                               onChange={this.handleChange}
                         />
-                    <div className="navbar-nav ml-auto">
+                    <div className="navbar-nav ml-auto navButton">
                         <button type="button" onClick={this.handleSubmit} className="btn btn-primary mr-2">Enter the storm!</button>
+                    </div>
+                    <div className="navbar-nav ml-auto navButton">
+                        <button type="button" onClick={this.navigateToResults} className="btn btn-default mr-2">Skip</button>
                     </div>
                     </form>
                 </div>
