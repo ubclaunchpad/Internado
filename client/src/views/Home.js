7d104a4ad26db57978db9ce8f855e4da80483c1a
steph <stephanie.wu@alumni.ubc.ca>
2018-11-11 10:59:22 -0800

    Change landing page button copy
    Changed non-button phrase copy to support button copy

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -31,7 +31,7 @@ export default class Home extends Component {
                     <p>Find your dream internship</p>
                     <br></br>
                     <br></br>
-                    <h5>Be the first to know about the newest product updates</h5>
+                    <h5>Stay up to date with the latest product developments and be one of the first to know when Internado is here</h5>
                     <form onSubmit={this.handleSubmit} className="my-8 my-lg-6">
                         <input className="form-control mr-sm-2"
                                type="email"
@@ -40,7 +40,7 @@ export default class Home extends Component {
                                onChange="5"
                         />
                     <div className="navbar-nav ml-auto">
-                        <button type="button" onClick={this.handleSubmit} className="btn btn-primary mr-2">Enter the storm!</button>
+                        <button type="button" onClick={this.handleSubmit} className="btn btn-primary mr-2">Join 0 other stormwatchers</button>
                     </div>
                     </form>
                 </div>
