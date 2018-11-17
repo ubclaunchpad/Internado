cb54e451638f8f6107f9644e954bb851a6768d86
Sherry Yuan <frostyyshadows@gmail.com>
2018-11-17 11:30:43 -0800

    added Pipfile

--- data_acquisition/data_acquisition.py
+++ data_acquisition/data_acquisition.py
@@ -11,27 +11,28 @@ import getpass
 
 # make ZipRecruiter API call
 API_KEY = os.getenv("ZIPRECRUITER_API_KEY")
-try:
-  req = requests.get("https://api.ziprecruiter.com/jobs/v1?search=internship%20Job&days_ago=1&page=1&api_key=" + API_KEY)
-  response = req.json()
-  if response["success"] == True:
-    # parse ZipRecruiter's JSON response
-    for job in response["jobs"]:
-        title = job["name"]
-        description = job["snippet"]
-        company = job["hiring_company"]["name"]
-        location = job["location"]
-        salaryMin = job["salary_min"]
-        postUrl = job["url"]
-        # add to database
-        con = psycopg2.connect(dbname='postgres', user=getpass.getuser(), host='localhost', password='Pa55word')
-        cur = con.cursor()
-        cur.execute(
-          "INSERT INTO Jobs VALUES(DEFAULT,'{title}','{description}','{company}','{location}',{salaryMin},'{postUrl}') " \
-          .format(title=title, description=description, company=company, location=location, salaryMin=salaryMin, postUrl=postUrl) \
-        )
-        con.commit()
-        cur.close()
-        con.close()
-except requests.ConnectionError:
-  print("You are not connected to the Internet")
+if API_KEY != None:
+  try:
+    req = requests.get("https://api.ziprecruiter.com/jobs/v1?search=internship%20Job&days_ago=1&page=1&api_key=" + API_KEY)
+    response = req.json()
+    if response["success"]:
+      # parse ZipRecruiter's JSON response
+      for job in response["jobs"]:
+          title = job["name"]
+          description = job["snippet"]
+          company = job["hiring_company"]["name"]
+          location = job["location"]
+          salaryMin = job["salary_min"]
+          postUrl = job["url"]
+          # add to database
+          con = psycopg2.connect(dbname='postgres', user=getpass.getuser(), host='localhost', password='Pa55word')
+          cur = con.cursor()
+          cur.execute(
+            "INSERT INTO Jobs VALUES(DEFAULT,'{title}','{description}','{company}','{location}',{salaryMin},'{postUrl}') " \
+            .format(title=title, description=description, company=company, location=location, salaryMin=salaryMin, postUrl=postUrl) \
+          )
+          con.commit()
+          cur.close()
+          con.close()
+  except requests.ConnectionError:
+    print("You are not connected to the Internet")
