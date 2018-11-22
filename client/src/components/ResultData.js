051bf20d760b682d91a01dae28606618156a25be
Michael <michaelkxian@gmail.com>
2018-11-21 19:13:52 -0800

    Change ResultsData to fit database schema
    The ResultsData object itself has been changed, but I still need to edit
    the ResultsTable to reflect this change and properly display the
    location.

--- client/src/components/ResultData.js
+++ client/src/components/ResultData.js
@@ -1,10 +1,19 @@
 export default class ResultData {
-  constructor(title, description, excerpt, location, postUrl, applyUrl) {
+  constructor(title, link, description, excerpt, city, state, country, latitude, longitude, companyName, minSalary, startDate) {
     this.title = title;
+    this.link = link;
     this.description = description;
     this.excerpt = excerpt;
-    this.location = location;
-    this.postUrl = postUrl;
-    this.applyUrl = applyUrl;
+    this.city = city;
+    this.state = state;
+    this.country = country;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.companyName = companyName;
+    this.location = "";
+    this.minSalary = minSalary;
+    this.startDate = startDate;
+    this.postUrl = link;
+    this.applyUrl = link;
   }
 }
