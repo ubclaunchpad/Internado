575e9bf7768fbd8fb8283a22b35030081bd0f53d
Michael <michaelkxian@gmail.com>
2018-11-23 14:08:02 -0800

    Make ResultsTable match database schema

--- client/src/components/ResultData.js
+++ client/src/components/ResultData.js
@@ -1,10 +1,19 @@
 export default class ResultData {
-  constructor(title, description, excerpt, location, postUrl, applyUrl) {
+  EXCERPTLENGTH = 100;
+  constructor(title, link, description, city, state, country, latitude, longitude, company_name, salary_min, start_date) {
     this.title = title;
+    this.link = link;
     this.description = description;
-    this.excerpt = excerpt;
-    this.location = location;
-    this.postUrl = postUrl;
-    this.applyUrl = applyUrl;
+    this.excerpt = excerpt = description.substring(0,EXCERPTLENGTH);
+    this.city = city;
+    this.state = state;
+    this.country = country;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.company_name = company_name;
+    this.location = "";
+    this.salary_min = salary_min;
+    this.start_date = start_date;
+    this.link = link;
   }
 }
