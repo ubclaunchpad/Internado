5cdab0b87b7916ce4fe5aaaef42d2eb94067818a
Ruperto Torres <rupertotorres1@gmail.com>
2019-01-20 14:24:22 -0800

    Adapted files in server/configuration and server/controllers to coding standards, plus some refactoring (#168)
    Adapted files in server/configuration and server/controllers to coding standards, plus some refactoring

--- server/controllers/mailingListController.ts
+++ server/controllers/mailingListController.ts
@@ -1,20 +1,22 @@
-import {Request, Response} from "express";
-import {Connection, getConnection} from "typeorm";
+import { Request, Response } from "express";
+import { Connection, getConnection } from "typeorm";
 import MailingListEntry from "../models/mailingListEntry";
 
-export async function addToMailingList(req: Request, res: Response): Promise<void> {
+export async function addToMailingList(
+    req: Request,
+    res: Response
+): Promise<void> {
     res.header("Access-Control-Allow-Origin", "*");
-    let connection: Connection = getConnection();
+    const connection: Connection = getConnection();
 
-    let email: string = req.query.email;
-    email = sanitizeEmail(email);
+    const email: string = sanitizeEmail(req.query.email);
 
     if (!isValidEmailAddress(email)) {
-        res.status(400).send({error: "Email address isn't valid"});
+        res.status(400).send({ error: "Email address isn't valid" });
         return;
     }
 
-    let listEntry: MailingListEntry = {email};
+    const listEntry: MailingListEntry = { email };
 
     try {
         await connection
@@ -24,34 +26,38 @@ export async function addToMailingList(req: Request, res: Response): Promise<voi
             .values([listEntry])
             .execute();
 
-        res.status(201).send({result: listEntry});
+        res.status(201).send({ result: listEntry });
     } catch (err) {
         if (err.code === "23505") {
-            res.status(400).send({error: "This email is already on the mailing list"});
+            res.status(400).send({
+                error: "This email is already on the mailing list"
+            });
         } else {
-            res.status(500).send({error: err});
+            res.status(500).send({ error: err });
         }
     }
 }
 
-export async function deleteFromMailingList(req: Request, res: Response): Promise<void> {
-    let connection: Connection = getConnection();
+export async function deleteFromMailingList(
+    req: Request,
+    res: Response
+): Promise<void> {
+    res.header("Access-Control-Allow-Origin", "*");
+    const connection: Connection = getConnection();
 
-    let email: string = req.query.email;
-    email = sanitizeEmail(email);
+    const email: string = sanitizeEmail(req.query.email);
 
     try {
         await connection
             .createQueryBuilder()
             .delete()
             .from(MailingListEntry)
-            .where("email = :email", {email})
+            .where("email = :email", { email })
             .execute();
 
-        res.header("Access-Control-Allow-Origin", "*");
-        res.status(200).send({result: `Removed ${email} from mailing list`});
+        res.status(200).send({ result: `Removed ${email} from mailing list` });
     } catch (err) {
-        res.status(500).send({error: err});
+        res.status(500).send({ error: err });
     }
 }
 
@@ -61,11 +67,13 @@ function sanitizeEmail(email: string): string {
 
 function isValidEmailAddress(email: string): boolean {
     // Regex taken from https://emailregex.com/
-    let regex = new RegExp(
-                "^(([^<>()\\[\\]\\.,;:\\s@\\\"]+" +
-                        "(\\.[^<>()\\[\\]\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))" +
-                        "@(([^<>()[\\]\\.,;:\\s@\\\"]+\\.)+" +
-                        "[^<>()[\\]\\.,;:\\s@\\\"]{2,})$", "gi");
-    let matches = email.match(regex);
+    const regex = new RegExp(
+        "^(([^<>()\\[\\]\\.,;:\\s@\\\"]+" +
+            "(\\.[^<>()\\[\\]\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))" +
+            "@(([^<>()[\\]\\.,;:\\s@\\\"]+\\.)+" +
+            "[^<>()[\\]\\.,;:\\s@\\\"]{2,})$",
+        "gi"
+    );
+    const matches = email.match(regex);
     return matches !== null && matches.length === 1;
 }
