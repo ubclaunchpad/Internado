0048acb5518b109fd0c746e7f24334304d37610f
ruperto <rupertotorres1@gmail.com>
2019-03-09 15:03:00 -0800

    delete server folder

--- server/models/searchRequest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var OrderBy;
-(function (OrderBy) {
-    OrderBy["Relevance"] = "relevance";
-    OrderBy["Distance"] = "distance";
-})(OrderBy = exports.OrderBy || (exports.OrderBy = {}));
-//# sourceMappingURL=searchRequest.js.map
