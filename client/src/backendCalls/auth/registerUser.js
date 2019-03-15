3e66dbf6e95819ad3015e620e3663e3f5178b6be
Michael <michaelkxian@gmail.com>
2019-03-15 14:49:22 -0700

    Implement registerUser

---
+++ client/src/backendCalls/auth/registerUser.js
@@ -0,0 +1,50 @@
+//import appConfig from '../../../../auth_server/src/configurations/app.ts';
+
+/**
+ *   Registers a user with the given email, password, first and last name.
+ *   If either first_name or last_name are undefined, the user will be registered
+ *   without a first or last name respectively.
+ */
+export async function registerUser(email, password, first_name, last_name) {
+  var details = {
+    "email": email,
+    "password": password,
+    "first_name": first_name,
+    "last_name": last_name
+  }
+
+  var formBody = [];
+  for (var property in details) {
+    if (details[property] === undefined) {
+      continue;
+    }
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+
+  const response = await fetch("http://localhost:5050/user", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formBody,
+    });
+
+    const body = await response.json();
+
+    console.log(body);
+    if (response.status !== 201) {
+      if (body.error) {
+        return body.error;
+      }
+      else {
+        return JSON.stringify(body);
+      }
+    } else {
+        return 200;
+    }
+}
+
+export default registerUser
