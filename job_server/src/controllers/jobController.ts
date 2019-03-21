99c7d13313dba9dfa30e63f895b8b2c5101d925f
Cowan <ctcumming@gmail.com>
2019-03-21 16:22:30 -0700

    Fixed incorrect job delete error and used our schema for posting jobs.

--- job_server/src/controllers/jobController.ts
+++ job_server/src/controllers/jobController.ts
@@ -2,18 +2,10 @@ import { Request, Response } from "express";
 import { getRepository, Repository } from "typeorm";
 import Job from "../models/job";
 
-function zipJobToJob(zipJob: any): Job {
+function reqJobToJob(reqJob: any): Job {
     let job: Job = new Job();
     job.id = undefined;
-    job.job_title = zipJob.name;
-    job.link = zipJob.url;
-    job.description = zipJob.snippet;
-    job.city = zipJob.city;
-    job.state = zipJob.state;
-    job.country = zipJob.country;
-    job.company_name = zipJob.hiring_company.name;
-    job.salary_min = zipJob.salary_min_annual;
-
+    job = {...job, ...reqJob};
     return job;
 }
 
@@ -22,7 +14,7 @@ export async function postJob(
     res: Response
 ): Promise<void> {
     res.header("Access-Control-Allow-Origin", "*");
-    let job: Job = zipJobToJob(req.body);
+    let job: Job = reqJobToJob(req.body);
 
     try {
         let jobsRepository: Repository<Job> = getRepository(Job);
@@ -46,7 +38,7 @@ export async function postJobs(
         return;
     }
 
-    let jobs: Job[] = req.body.map(zipJobToJob);
+    let jobs: Job[] = req.body.map(reqJobToJob);
 
     try {
         let jobsRepository: Repository<Job> = getRepository(Job);
@@ -80,7 +72,7 @@ export async function deleteJob(
     try {
         let jobsRepository: Repository<Job> = getRepository(Job);
         let job: Job = await jobsRepository.findOne(req.query.id);
-        if (job === null) {
+        if (!job) {
             res.status(400).send({error: `Job id ${req.query.id} not found`});
             return;
         }
